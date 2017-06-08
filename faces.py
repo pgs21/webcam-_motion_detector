d93f9514fc2e2300a409c18bacdf175c6d4fb0cb
pgs <daniel.rbdr@gmail.com>
2017-06-08 10:53:49 +0100

    flipped image horizontaly added black borders

--- faces.py
+++ faces.py
@@ -9,15 +9,17 @@ firstFrame = None
 face_detected = 0
 frame_count = 0
 
-blank_image = np.zeros((480,640,3), np.uint8)
+blank_image = np.zeros((480,854,3), np.uint8)
 
 cv2.namedWindow("Frame Delta", cv2.WND_PROP_FULLSCREEN)          
 cv2.setWindowProperty("Frame Delta", 0, 1)
 
 while(1):
 	ret, img = cap.read()
+	img = cv2.flip(img,1)
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	grayblur = cv2.GaussianBlur(gray, (21, 21), 0)
+
 
 	# if the first frame is None, initialize it
 	if firstFrame is None:
@@ -31,12 +33,14 @@ while(1):
 
 	if (face_detected >0 ):
 		frameDelta = cv2.absdiff(firstFrame, grayblur)
-		cv2.imshow("Frame Delta", frameDelta)
+		border=cv2.copyMakeBorder(frameDelta, top=0, bottom=0, left=107, right=107, borderType= cv2.BORDER_CONSTANT, value=[0,0,0] )
+		cv2.imshow("Frame Delta", border)
 		face_detected -=1
 	cv2.waitKey(1)& 0xff
 	for dorme in range(0,15):
 		cap.read()
 	cv2.imshow("Frame Delta", blank_image)
+	cv2.waitKey(1)& 0xff
 	for dorme2 in range(0,1):
 		cap.read()
 
